7d9ef4af0b8ef7ea32864275322d0065d95f3521
KhallilB <khallil.bailey@gmail.com>
2020-03-16 11:20:56 -0700

    [add] passport and cors

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+// Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
+require('dotenv').config();
+require('./config/passport/index');
+
+// Initalize
 const server = express();
 
+// Middleware
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(passport.initialize());
+server.use();
 
+// Port
 const port = process.env.PORT;
+
+// Connect server
 server.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
