c8971d8b81efe80fe8c6a6cf240975810f4f8f7c
KhallilB <khallil.bailey@gmail.com>
2020-03-16 12:15:42 -0700

    [add] pre-save hook to hash password

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-const User = new Schema({
+const UserSchema = new Schema({
   firstName: {
     type: String,
     trim: true,
@@ -41,3 +42,34 @@ const User = new Schema({
     default: false
   }
 });
+
+// Pre-save hook thatsalts and hashes User passwords save salt secret string
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  // Only hash password if it has been modified or new
+  if (!user.isModified('password')) return next();
+
+  // Generate salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    // Hash the password
+    // eslint-disable-next-line no-shadow
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // Override password with hashed one
+      user.password = hash;
+      user.saltSecret = salt;
+      next();
+    });
+  });
+});
+
+// So bcrypt doesnt compare hashed password
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
